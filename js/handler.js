e8fe290ff30db91bf0562502480480fac8137912
Johnson <johnson780510@gmail.com>
2013-07-31 00:54:34 +0800

    @ 重要 變更 .replyCount 位置
    把 左下角的 .replyCount  移動到 右上角
    變成  [版名][回復數量][分數]

--- js/handler.js
+++ js/handler.js
@@ -16,7 +16,7 @@ var context = {
     NAME: "",
     CONTENT: "不會游泳，現在要進去海陸新訓了........<br>感覺這一年當兵生活會很慘",
     imgNo: "01",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "32"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 45623,
@@ -25,7 +25,7 @@ var context = {
     NAME: "ㄈㄓZ",
     CONTENT: "李嫌和王女原本約在高鐵台南站見面，因鄰居發現王母失蹤",
     imgNo: "02",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "2"
   },{
   	BOARDNAME: "新聞速報",
   	POINT: "78,645,456",
@@ -34,7 +34,7 @@ var context = {
     NAME: "",
     CONTENT: "外來種賤女狗公惱羞開始血尿跳針哭哭ㄛwwwwww<br>護航乙",
     imgNo: "03",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "8"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 456456,
@@ -43,7 +43,7 @@ var context = {
     NAME: "",
     CONTENT: "搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李w好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李搞不好李嫌跟王女真的有交往過<br>因為某某原因而否認了<br>就跟以前認識某女明明就有暗示我告白<br>結果我沒告白就到處抹黒我說我有嚴重妄想症一樣",
     imgNo: "04",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "21"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 456,
@@ -52,7 +52,7 @@ var context = {
     NAME: "",
     CONTENT: "這麼簡單的事?<br>那勇者都不用當勇者了",
     imgNo: "",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "5"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 14566,
@@ -61,7 +61,7 @@ var context = {
     NAME: "",
     CONTENT: "肥宅果然只會在這嘴砲wwwwwwwww",
     imgNo: "06",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "456456432"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 78956,
@@ -70,7 +70,7 @@ var context = {
     NAME: "",
     CONTENT: "你是衛生棉我是經血",
     imgNo: "07",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "12"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 79,
@@ -79,7 +79,7 @@ var context = {
     NAME: "",
     CONTENT: "剛剛在巴哈看到一個設定廚在寫小說<br>訂了一堆中二的設定= =",
     imgNo: "",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "25"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 5,
@@ -88,7 +88,7 @@ var context = {
     NAME: "09",
     CONTENT: "幹!!!! animate有夠多人<br>早上九點半過去看,<br>一群肥宅油腐就已經開始擠在外面排隊了<br>害我差點被油煙悶死",
     imgNo: "09",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "38"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 76,
@@ -97,7 +97,7 @@ var context = {
     NAME: "",
     CONTENT: "漫畫買了很佔位子<br>外加跟我迷的人也不講什麼義氣只會凹人",
     imgNo: "10",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "45"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 78623,
@@ -106,7 +106,7 @@ var context = {
     NAME: "",
     CONTENT: "野生的外來種?<br>賣ACG周邊&原版漫畫&同人誌",
     imgNo: "11",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "44"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 78656456,
@@ -115,7 +115,7 @@ var context = {
     NAME: "",
     CONTENT: "那個是來釣我話的你被釣幹麻",
     imgNo: "12",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "62"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 56,
@@ -124,7 +124,7 @@ var context = {
     NAME: "",
     CONTENT: "實體店面不都比較貴嗎?<br>網拍買不就好了",
     imgNo: "13",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "42"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 1789,
@@ -133,7 +133,7 @@ var context = {
     NAME: "",
     CONTENT: "夜店台女有福囉~抗告成功、土耳其男大生今可望交保",
     imgNo: "",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "12"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 353453,
@@ -142,7 +142,7 @@ var context = {
     NAME: "",
     CONTENT: "無本文",
     imgNo: "15",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "84"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 453,
@@ -151,7 +151,7 @@ var context = {
     NAME: "",
     CONTENT: "為什麼支那賤畜一聽到台灣女生說話就射啦<br>我剛剛無聊上百度搜尋台灣女生，發現很多426很哈台灣的劍",
     imgNo: "16",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "32"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 123456,
@@ -160,7 +160,7 @@ var context = {
     NAME: "",
     CONTENT: "問一下 問答板為什麼只能設3個問題? 我看其他人都25題耶",
     imgNo: "17",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "4"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 453453,
@@ -169,7 +169,7 @@ var context = {
     NAME: "",
     CONTENT: "有島民學過速讀嗎?<br>我想要自學卻不知道要怎麼入門",
     imgNo: "18",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "3"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 51245,
@@ -178,7 +178,7 @@ var context = {
     NAME: "",
     CONTENT: "有島民知道拿熱水沖屁眼完5分鐘在拿冷水沖是什麼感覺嗎",
     imgNo: "",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "0"
   },{
   	BOARDNAME: "綜合二",
   	POINT: 7857853435,
@@ -187,7 +187,7 @@ var context = {
     NAME: "",
     CONTENT: "有什麼地方可以吹冷氣又有插座?",
     imgNo: "20",
-    REPLYCOUNT: "432 回應"
+    REPLYCOUNT: "2"
   }]
 };
 Handlebars.registerHelper('fullName', function(person) {
